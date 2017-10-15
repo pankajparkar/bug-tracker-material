112d0d3528c4e3e04dc3842d32004b57d24483ca
Pankaj Parkar <parkarpankaj3@gmail.com>
2017-10-16 00:25:49 +0530

    Conditionally closed sidebar.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'bt-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'bt';
+export class AppComponent implements OnInit {
+
+  url: string;
+  constructor(private router: Router){ }
+
+  openSidebar(){
+    return this.url === '/dashboard';
+  }
+
+  ngOnInit(){
+    this.router.events
+    .subscribe(event=>{
+      if(event instanceof NavigationEnd){
+        this.url = event.url;
+      }
+    });
+  }
 }
